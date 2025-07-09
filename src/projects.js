caad2cd486cdb792d3a3123d8a894da296e2309a
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-07-09 18:30:11 +0200

    Remove dateOfWeek in projects
    The date of week was giving me problems to execute.

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,11 @@
-import { format } from "date-fns";
+import { format, startOfWeek, endOfWeek } from "date-fns";
 
 export class Project {
-    constructor(title, dueDate, why, dateOfWeek, weeksToGoInTheYear, goalsOfWeek, weekDay, dailyTasksOfGoals) {
+    constructor(title, dueDate, why, weeksToGoInTheYear, goalsOfWeek, weekDay, dailyTasksOfGoals) {
         this.title = title;
         this.dueDate = format(new Date(dueDate), "dd/MM/yyyy");
         this.why = why;
         this.weekOfYear = format(new Date(), "ww");
-        this.dateOfWeek = dateOfWeek;
         this.weeksToGoInTheYear = weeksToGoInTheYear;
         this.goalsOfWeek = goalsOfWeek;
         this.weekDay = weekDay;
@@ -17,7 +16,7 @@ export class Project {
         return `${this.title}
         Due: ${this.dueDate}
         The Why: ${this.why}
-        Current Week of Year: Week ${this.weekOfYear} - ${this.dateOfWeek} (${this.weeksToGoInTheYear} weeks left before goal expires)
+        Current Week of Year: Week ${this.weekOfYear} (${this.startOfWeek} - ${this.endOfWeek}) (${this.weeksToGoInTheYear} weeks left before goal expires)
         Goals of Week: ${this.goalsOfWeek}
         ${this.weekDay}:
         ${this.dailyTasksOfGoals}`;

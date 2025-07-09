7edda0ec9396f08490f6d3188701181b43c9a17b
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-07-09 11:22:05 +0200

    Create priority class

--- src/home.js
+++ src/home.js
@@ -13,3 +13,18 @@
         return;
     }
 }
+
+class Priority {
+    constructor(critical, high, medium, low, lowest) {
+        this.critical = critical;
+        this.high = high;
+        this.medium = medium;
+        this.low = low;
+        this.lowest = lowest;
+    }
+
+    get priority() {
+        //this is supposed to come out as a list
+        
+    }
+}

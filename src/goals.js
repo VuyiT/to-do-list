4acd7b3fbf2ca27ca10d4372dc2b2a11134fab26
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-07-09 10:05:26 +0200

    Create a goals js file

---
+++ src/goals.js
@@ -0,0 +1,8 @@
+class createGoal {
+    constructor(title, why, dueDate, tasks) {
+        this.title = title;
+        this.why = why;
+        this.dueDate = dueDate;
+        this.tasks = tasks;
+    }
+}

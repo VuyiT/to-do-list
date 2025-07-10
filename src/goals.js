ed2fbef5658ef04094bb7e3b1453f4df00591f64
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-07-10 09:00:04 +0200

    Delete goals.js
    I will no longer be needing this functionality as tasks will fall directly under projects and I will track them under projects.

--- src/goals.js
+++
@@ -1,8 +0,0 @@
-class createGoal {
-    constructor(title, why, dueDate, tasks) {
-        this.title = title;
-        this.why = why;
-        this.dueDate = dueDate;
-        this.tasks = tasks;
-    }
-}

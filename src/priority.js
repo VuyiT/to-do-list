98748da808dbbb5f691aa8bcc699d0ea20b02219
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-07-10 09:48:35 +0200

    Move priority to own mod

---
+++ src/priority.js
@@ -0,0 +1,14 @@
+class Priority {
+    constructor(critical, high, medium, low, lowest) {
+        this.critical = critical;
+        this.high = high;
+        this.medium = medium;
+        this.low = low;
+        this.lowest = lowest;
+    }
+
+    get priority() {
+        //this is supposed to come out as a list
+        
+    }
+}
--- src/tasks.js
+++ src/tasks.js
@@ -16,18 +16,3 @@ export class Task {
         return;
     }
 }
-
-class Priority {
-    constructor(critical, high, medium, low, lowest) {
-        this.critical = critical;
-        this.high = high;
-        this.medium = medium;
-        this.low = low;
-        this.lowest = lowest;
-    }
-
-    get priority() {
-        //this is supposed to come out as a list
-        
-    }
-}
